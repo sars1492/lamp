02c7028cfd9a7f1cb4ad8689ce38c3e713f0c19f
Juraj Szasz <juraj.szasz3@gmail.com>
2017-02-03 12:26:36 +0100

    Correct some errors

--- lamp.sh
+++ lamp.sh
@@ -1,32 +1,65 @@
 #!/bin/bash
 #Linux Audio Media Player (LAMP)
-#Version 0.5 by Bond (George Szasz)
+#Version 0.5 by Bond (Juraj Szasz)
 
-PID=`cat < ~/.pid`			#Read old PID
+if [ ! -e ~/.lamp/pid ]; then		#make new pidfile
+  mkdir ~/.lamp/
+  echo $$ > ~/.lamp/pid
+fi
+PID=`cat < ~/.lamp/pid`			#read old pid
+echo $PID
 
 #Read Arguments
 for ((NARG=1; NARG<=$#; NARG++))
 do
   if [ "${!NARG}" = "-r" -o "${!NARG}" = "repeat" ]; then
     REPEAT=t
+    OLDLIST=t
   elif [ "${!NARG}" = "-rt" -o "${!NARG}" = "repeat-track" ]; then
     REPTRACK=t
+    OLDLIST=t
+  elif [ "${!NARG}" = "-f" -o "${!NARG}" = "foward" ]; then
+    FOWARD=t
+    OLDLIST=t
+  elif [ "${!NARG}" = "-b" -o "${!NARG}" = "backward" ]; then
+    BACKWARD=t
+    OLDLIST=t
+  elif [ "${!NARG}" = "-sh" -o "${!NARG}" = "shufle" ]; then
+    SHUFLE=t
+    OLDLIST=t
+  elif [ "${!NARG}" = "-p" -o "${!NARG}" = "pause" ]; then
+    if [ "`ps --no-headers -O -p $PID |  gawk '{ print $3 }' -`" = "T" ]; then
+      kill -CONT -$PID &> /dev/null
+    else
+      kill -STOP -$PID &> /dev/null 
+    fi
+    exit
   elif [ "${!NARG}" = "-s" -o "${!NARG}" = "stop" ]; then
     kill -- -$PID 
+    rm -fr ~/.lamp/
     exit
   elif [ "${!NARG}" = "-h" -o "${!NARG}" = "help" ]; then
-    echo "Použitie: lamp [nastavenia]"
-    echo "-p, play n		skociť na n-tú skladbu"
-    echo -e "-s, stop		\tukončiť program"
-    echo "-g, gain n		nastaviť n-tú hlasitosť (v percentách)"
-    echo "-r, repeat		opakovať zvolené skladby"
-    echo "-rt, repeat-trakc	opakovať určitú skladbu"
-    echo -e "-o, open SÚBOR	\tčítať playlist zo SÚBORu"
-    echo -e "-w, write SÚBOR	\tzapísať playlist do SÚBORu"
+    echo "Použitie: lamp [nastavenia] &"
+    echo "-t n, track n			skociť na n-tú skladbu"
+    echo "-f, foward			skociť na naskedujúcu skladbu"
+    echo -e "-b, backward		\tskociť na prechádzajúcu skladbu"
+    echo "-s, stop			zastaviť prehrávanie"
+    echo "-p, pause			prerusiť prehrávanie"
+    echo "-g, gain n			nastaviť hlasitosť na n (v percentách)"
+    echo "-r, repeat			opakovať zvolené skladby"
+    echo "-rt, repeat-track		opakovať určitú skladbu"
+    echo "-sh, shufle			nahodne hrať zvolené skladby"
+    echo -e "-o, open SÚBOR		\tčítať playlist zo SÚBORu"
+    echo -e "-w, write SÚBOR		\tzapísať playlist do SÚBORu"
+    echo -e "-d, directory ADRESÁR	\thrať v ADRESÁRi"
     exit
-  elif [ "${!NARG}" = "-p" -o "${!NARG}" = "play" ]; then
+  elif [ "${!NARG}" = "-d" -o "${!NARG}" = "directory" ]; then
+    shift
+    cd "${!NARG}"
+  elif [ "${!NARG}" = "-t" -o "${!NARG}" = "track" ]; then
     shift
     TRACK=${!NARG}
+    OLDLIST=t
   elif [ "${!NARG}" = "-g" -o "${!NARG}" = "gain" ]; then
     shift
     aumix -w ${!NARG}
@@ -44,7 +77,27 @@ do
     exit
   fi
 done
-#echo 1
+#echo a
+
+play() {
+  
+  FILE=$1
+  REPTRACK=$2
+
+  while true
+  do
+    echo 11
+    #echo "$FILE"
+    if [ "${FILE%%*.mp3}" = "$NULL" ]; then
+      mpg123 -q "$FILE" 2> /dev/null
+    elif [ "${FILE%%*.ogg}" = "$NULL" ]; then
+      ogg123 -q "$FILE" 2> /dev/null 
+    fi
+    if [ -z "$REPTRACK" ]; then
+      break
+    fi
+  done
+}
 
 #Make new playlist
 if [ -n "$WLIST" ]; then
@@ -59,58 +112,99 @@ elif [ -n "$WRITE" ]; then
   exit
 fi
 
-#If Playlist non-exsist
-if [ ! -e "$RLIST" -a -n "$RLIST" ]; then
-  echo lamp: súbor $RLIST neexistuje
-  exit
-fi
 
-#If track aren't in local directory
-if [ -z "$RLIST" -a -z "`ls | grep mp3$`" -a -z "`ls | grep ogg$`" ]; then
-  echo "lamp: V lokálnom adresári nie sú žiadne ogg-y alebo mp3-ky"
-  echo "Presuňte sa do adresara s príslušnými súbormi alebo"
-  echo "použite Playlist prikazom \`lamp -o Playlist'"
-  exit
-fi
 
 #Kill old instances
 if [ -n "`ps x | grep 'mpg123\|ogg123' | grep -v grep`" -o -n "`ps x | grep $PID | grep -v grep`" ]; then
   #echo 2
   kill -- -$PID && killall -wq ogg123 
+  if [ -z "$RLIST" -a -z "$OLDLIST" ]; then
+    rm -fr ~/.lamp
+  fi
 fi
-echo $$ > ~/.pid			#Write new PID	
+echo $$ > ~/.lamp/pid				#Write new PID	
 
-#echo 4
+#echo b
 #Playing iteration
 while true
 do
-  #echo 5
+  echo c
   #echo $RLIST
-  #A=`list -r $RLIST`
-  for FILE in `list -r $RLIST`
+  #echo $WARD
+  if [ ! -e "$RLIST" -a -n "$RLIST" ]; then	#If Playlist non-exsist
+    echo lamp: súbor $RLIST neexistuje
+    exit
+  elif [ -z "$RLIST" -a -z "$OLDLIST" ]; then
+    #echo list
+    ls | grep 'mp3$\|ogg$' | list -w ~/.lamp/playlist
+    RLIST=~/.lamp/playlist
+  elif [ -e ~/.lamp/playlist ]; then
+    RLIST=~/.lamp/playlist
+    #echo e
+  elif [ -n "$RLIST" ]; then
+    cat "$RLIST" > ~/.lamp/playlist
+  fi
+
+  #cat $RLIST
+  #If track aren't in local directory
+  if [ "`cat $RLIST`" = "$PWD/mp3" ]; then
+    echo "lamp: V lokálnom adresári nie sú žiadne ogg-y alebo mp3-ky"
+    echo "Presuňte sa do adresara s príslušnými súbormi alebo"
+    echo "použite Playlist prikazom \`lamp -o Playlist'"
+    exit
+  fi
+
+  #echo $RLIST
+  MAX=`list -l "$RLIST"`
+  MAX=`expr $MAX + 1`
+  for ((I=1; I<=$MAX; I++))
   do
-    #echo "$FILE"
-    #echo 6
+    #echo 8
     #echo $REPEAT
-    FILE=`list -l "$FILE"`
-    if [ "$TRACK" != "1" -a -n "$TRACK" ]; then
-      TRACK=`expr $TRACK - 1`
-      continue
+    FILE="`list -r "$RLIST" $I`"
+    #echo I$I
+    #echo "$FILE"
+    if [ -e ~/.lamp/track ]; then
+      TR=`cat ~/.lamp/track`
+    else
+      touch ~/.lamp/track
+      TR=1
+      echo track
     fi
-    while true
-    do
-      if [ "${FILE%%*.mp3}" = "$NULL" ]; then
-	mpg123 -q "$FILE" 2> /dev/null 
-      elif [ "${FILE%%*.ogg}" = "$NULL" ]; then
-	ogg123 -q "$FILE" 2> /dev/null 
-      fi
-      if [ -z "$REPTRACK" ]; then
-	break
-      fi
-    done
+    #echo R$TR
+    if [ -n "$SHUFLE" ];then
+      TRACK=`list -s ~/.lamp/seed $MAX`
+    elif [ -n "$FOWARD" ];then
+      TRACK=`expr $TR + 1`
+    elif [ -n "$BACKWARD" ];then
+      TRACK=`expr $TR - 1`
+    fi
+    #echo T$TRACK
+
+    #Repeat session
+    if [ -n "$REP" -a -z "$TRACK" ];then
+      TRACK=$TR
+    fi
+
+    if [ ! -e "$FILE" ];then
+      echo "súbor $FILE neexistuje"
+      echo "Pozrite či ste v playliste neurobili chybu"
+      exit
+    elif [ -z "$TRACK" ];then
+      TRACK=1
+      play "$FILE" $REPTRACK
+    elif [ $I -lt $TRACK ]; then
+      continue
+    else
+      echo $I > ~/.lamp/track
+      play "$FILE" $REPTRACK
+    fi
   done
+
   if [ -z "$REPEAT" ]; then
     #echo 6
     break
   fi
 done
+
+rm -fr ~/.lamp
